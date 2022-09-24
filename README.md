569d643c3878833b6e7c749a264afc11f0f27301
ainz <ainz713@gmail.com>
2022-09-24 19:14:22 +0300

    1. Техническое задание - проект доставка еды "Быстро и в точку"

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://app.travis-ci.com/ainz713/job4j_fast_food.svg?branch=master)](https://app.travis-ci.com/ainz713/job4j_fast_food)
+# job4j_fast_food
+
+Сервис быстрого питания
---
+++ src/main/java/com/example/job4j_fast_food/Job4jFastFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.job4j_fast_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jFastFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jFastFoodApplication.class, args);
+    }
+
+}
